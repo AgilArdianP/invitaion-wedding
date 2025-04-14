6aefdc0096a3bf275c7d3e41bd299ef166b0d482
AgilArdian <agilardian25@example.com>
2025-04-15 00:12:42 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wedding-invitaion/',
+  base: '/invitaion-wedding/',
   server: {
     port: 3000,
   },
